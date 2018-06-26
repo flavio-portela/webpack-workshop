2c8e9ff0d9f9b153e18e475db4f361b4c774c621
Flavio Portela <fportela@nearsoft.com>
2018-06-25 20:39:55 -0700

    removed compression

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const compression = require("compression");
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8888;
 
-app.use(compression());
 app.use(express.static("dist"));
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
